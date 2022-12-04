2956d8108ae230df8dd779f01700a22fe06d5d56
eomgichan <101166190+eomgichan@users.noreply.github.com>
2022-12-04 18:47:08 +0900

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,32 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeFirestore} from 'firebase/firestore';
+import { initializeFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
-  apiKey: "AIzaSyBpGtyIqEBVYnR6ZtF1-SQqqAa_HSHNUFs",
-
-  authDomain: "math-learning-app-5729e.firebaseapp.com",
-
-  projectId: "math-learning-app-5729e",
-
-  storageBucket: "math-learning-app-5729e.appspot.com",
-
-  messagingSenderId: "786257694735",
-
-  appId: "1:786257694735:web:cc092138160e4d7c3d937a"
-
+  apiKey: "AIzaSyB3WmTyw5wD3ND9FDhWp0pJKP47LP1cHE8",
+  authDomain: "teacher-information-ee9a5.firebaseapp.com",
+  projectId: "teacher-information-ee9a5",
+  storageBucket: "teacher-information-ee9a5.appspot.com",
+  messagingSenderId: "420985440995",
+  appId: "1:420985440995:web:c54e3d4fbe1e95ac6293d6"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = initializeFirestore(app, {
-    experimentalForceLongPolling:true,
+    expoerimentalForceLongPolling: true,
 });
 
-export default app
 export {db}
