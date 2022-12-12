c5d2225dbcb166374132dfaa199bdeb319c57a99
eomgichan <101166190+eomgichan@users.noreply.github.com>
2022-12-13 04:11:24 +0900

    Update Signup.js

--- Pages/Signup.js
+++ Pages/Signup.js
@@ -1,22 +1,25 @@
-import {TouchableOpacity, Text, View, TextInput, Button} from 'react-native';
+import {TouchableOpacity, Text, View, TextInput, Image, ImageBackground} from 'react-native';
 import {useState} from 'react';
 import {db} from '../firebaseConfig'
 import {
     addDoc, collection, getDocs,
      doc, updateDoc, where, query} from "firebase/firestore";
+import signup from '../assets/Signup.png'
 
-const SignUp = (props) => {
-    const [Flag,setFlag] = useState(true);
-    const [Id, setID] = useState("");
-    const [Password, setPassword] = useState("");
-    const [TeacherInfo, setTeacherInfo] = useState();
-    
+const Find = (props) => {
+    const [flag,setFlag] = useState(true);
+    const [name, setName] = useState("");
+    const [PhoneNumber, setPhoneNumber] = useState("");
+    const [ID, setID] = useState();
+    const [password, setPassword] = useState();
+    const [teacherInfo, setStudentInfo] = useState();
+    const [email, setEmail] = useState("");
 
     const readfromDB = async() => {
         try{
-            const data = await getDocs(collection(db, "Teacher Infomation"))
+            const data = await getDocs(collection(db, "teacher"))
             
-            setTeacherInfo(data.docs.map(doc=>(
+            setStudentInfo(data.docs.map(doc=>(
                 {...doc.data(), id: doc.id}
                 )))
         } catch(error) {
@@ -26,24 +29,44 @@ const SignUp = (props) => {
 
     const changeID = (event) => {
         setID(event)
-      }
+    }
+
     const changePassword = (event) => {
         setPassword(event)
     }
-    if (Flag) {
+
+    const changeName = (event) => {
+        setName(event)
+    }
+
+    const changeEmail = (event) => {
+        setEmail(event)
+    }
+
+    const changePhoneNumber = (event) => {
+        setPhoneNumber(event)
+    }
+    if (flag) {
         readfromDB()
         setFlag(false)
     }
 
     const signupteacher = async () => {
-        let idList = []
-        TeacherInfo?.map((item) => {
-            idList.push(item.teacher_id)
+        let infoList = []
+        teacherInfo?.map((item) => {
+            infoList.push(item.teacher_id)
+            infoList.push(item.password)
+            infoList.push(item.name)
+            infoList.push(item.PhoneNumber)
+            infoList.push(item.email)
         })
         try {
-            await addDoc(collection(db, "Teacher Information"), {
-                teacher_id : Id,
-                Password : Password,
+            await addDoc(collection(db, "teacher"), {
+                teacher_id : ID,
+                Password : password,
+                name : name,
+                PhoneNumber : PhoneNumber,
+                email : email
             })
             alert("Sign Up Complete!")
             props.navigation.navigate("Login")
@@ -54,21 +77,100 @@ const SignUp = (props) => {
 
     return (
         <View>
-            <Text>enter your ID</Text>
-            <TextInput
-                value = {Id}
-                onChangeText = {changeID}
-            />
-            <Text>enter your Password</Text>
-            <TextInput
-                value = {Password}
-                onChangeText = {changePassword}
-            />
+            <View
+                style ={{marginTop:130, marginLeft :'35%', marginRight:20, backgroundColor:'#FBFAFA', width: 390, height:300}}>
+                <Text
+                    style={{marginLeft:10, fontSize:15}}
+                >Enter your ID</Text>
+                <TextInput
+                    value = {ID}
+                    onChangeText = {changeID}
+                    style ={{marginLeft:10, width:'100%', marginTop:10}}
+                />
+                <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                    marginLeft:10,
+                    marginRight:10
+                }}
+                />
+                <Text
+                    style={{marginLeft:10, marginTop:10, fontSize:15}}
+                >Enter your Password</Text>
+                <TextInput
+                    value = {password}
+                    onChangeText = {changePassword}
+                    style ={{marginLeft:10, width:'100%', marginTop:10}}
+                />
+                <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                    marginLeft:10,
+                    marginRight:10
+                }}
+                />
+                <Text
+                    style={{marginLeft:10, fontSize:15}}
+                >Enter your Name</Text>
+                <TextInput
+                    value = {name}
+                    onChangeText = {changeName}
+                    style ={{marginLeft:10, width:'100%', marginTop:10}}
+                />
+                <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                    marginLeft:10,
+                    marginRight:10
+                }}
+                />
+                <Text
+                    style={{marginLeft:10, fontSize:15}}
+                >Enter your Email</Text>
+                <TextInput
+                    value = {email}
+                    onChangeText = {changeEmail}
+                    style ={{marginLeft:10, width:'100%', marginTop:10}}
+                />
+                <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                    marginLeft:10,
+                    marginRight:10
+                }}
+                />
+                <Text
+                    style={{marginLeft:10, fontSize:15}}
+                >Enter your Phone Number</Text>
+                <TextInput
+                    value = {PhoneNumber}
+                    onChangeText = {changePhoneNumber}
+                    style ={{marginLeft:10, width:'100%', marginTop:10}}
+                />
+                <View
+                style={{
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                    marginLeft:10,
+                    marginRight:10
+                }}
+                />
+            </View>
             <TouchableOpacity
-                title = "Sign Up"
-                onPress={signupteacher}
-            />
+                style = {{width:100}}
+                onPress={signupteacher}>
+                    <Image
+                        style={{width:390, height:100, marginLeft:'550%'}}
+                        source={signup}
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
         </View>
     );
 }
-export default SignUp
+
+export default Find
